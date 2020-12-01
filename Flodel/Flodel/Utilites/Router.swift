3bccd1d99cfe7ffb7cd00b1f2e82f5e7c9827d5a
mohammediskander <idarkcry@icloud.com>
2020-12-01 15:02:57 +0300

    Add error response status type

--- Flodel/Flodel/Utilites/Router.swift
+++ Flodel/Flodel/Utilites/Router.swift
@@ -75,3 +75,97 @@ enum AccessTypeError: Error {
 
 typealias Parameters = [String: Any]
 typealias QueryParameters = [String: String]
+
+enum ParametersError: Error {
+    case encodingError
+    case encoderError
+}
+
+/// Success response status
+enum SuccessResponseStatus: Int {
+    case ok = 200
+    case created
+    case accepted
+    case nonAuthoritativeInformation
+    case noContent
+    case resetContent
+    case partialContent
+    case multiStatus
+    case alreadyReported
+}
+
+/// Error response status
+enum ErrorResponseStatus: Int {
+    // MARK:- Flicker API error response status
+    case requiredArgugmentMissing = 1
+    case notAValidLatitude
+    case notAValidLongitude
+    case notAValidAccuracy
+    case SSLIsRequired = 95
+    case invalidSignature
+    case missingSignature
+    case loginFailedOrInvalidAuthToken
+    case userNotLoggedInOrInsufficientPermissions
+    case InvalidAPIKey
+    case serviceCurrentlyUnavailable
+    case writeOperationFailed
+    case formatNotFound = 111
+    case methodNotFound
+    case invalidSOAPEnvelope = 114
+    case invalidXMPRPCMethodCall
+    case badURLFound
+    
+    // MARK:- Redirection response status
+    case multipleChoices = 300
+    case movedPermanently
+    case found
+    case seeOther
+    case notModified
+    case useProxy
+    case temporaryRedirect
+    case permanentRedirect
+    
+    // MARK:- Client error response status
+    case badRequest = 400
+    case unauthorized
+    case paymentRequired
+    case forbidden
+    case notFound
+    case methodNotAllowed
+    case notAcceptable
+    case proxyAuthenticationRequired
+    case requestTimeout
+    case conflict
+    case gone
+    case lengthRequried
+    case preconditionFailed
+    case payloadTooLarge
+    case requestURITooLong
+    case unsupportedMediaType
+    case requestedRangeTotSatisfiable
+    case exceptionFailed
+    case misdirectedRequest = 421
+    case unprocessableentity
+    case locked
+    case failedDependency
+    case upgradeRequired = 426
+    case preconditionRequired = 428
+    case tooManyRequestes
+    case requestHeaderFieldsTooLarge = 431
+    case connectionCloseWithoutResponse = 44
+    case unavaliableForLegalResons = 451
+    case clientClosedRequest = 499
+    
+    //MARK:- Server error response status
+    case internalServerError = 500
+    case notImplemented
+    case badGateway
+    case serviceUnavailable
+    case httpVersionNotSupported
+    case variantAlsoNegotiates
+    case insufficientStorage
+    case loopDetected
+    case notExtended
+    case networkAuthenticationRequired
+    case networkConnectTimeoutError = 599
+}
